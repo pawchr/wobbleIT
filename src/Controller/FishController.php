6ad1a2ae979a9b18910e6cd58ce7a86e8bd0ba86
pawel <pawladz@gmail.com>
2025-06-29 21:21:15 +0200

    add: fish validation

--- src/Controller/FishController.php
+++ src/Controller/FishController.php
@@ -2,28 +2,32 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Form\FormFactoryInterface;
-use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Fish;
 use App\Entity\FishSpecies;
-use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Fishing;
+use App\Validator\FishValidator;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Form\FormError;
 
 final class FishController extends AbstractController
 {
-    #[Route('/fish/add', name:'app_fish_add')]
-    public function add(
-        FormFactoryInterface $formFactory,
-        Request $request,
-        EntityManagerInterface $em
-    ) : Response {
-            $form = $formFactory->createBuilder()
+    public function __construct(
+        private FormFactoryInterface $formFactory,
+        private EntityManagerInterface $em,
+        private FishValidator $fishValidator,
+    ) {}
 
+    #[Route('/fish/add', name: 'app_fish_add')]
+    public function add(Request $request): Response
+    {
+        $form = $this->formFactory->createBuilder()
             ->add('species', EntityType::class, [
                 'class' => FishSpecies::class,
                 'choice_label' => 'name',
@@ -37,27 +41,33 @@ final class FishController extends AbstractController
                 'attr' => ['placeholder' => 'Length'],
             ])
             ->getForm();
+              
+            $form->handleRequest($request);
 
-        $form->handleRequest($request);
+            if ($form->isSubmitted()) {
+                $data = $form->getData();
+                $fish = new Fish();
+                $fish->setLength((float) $data['length']);
+                $fish->setSpecies($data['species']);
+                $activeFishing = $this->em->getRepository(Fishing::class)->findOneBy(['active' => true]);
+                $fish->setFishing($activeFishing);
+                
+                if (!$activeFishing) {
+                    $form->addError(new FormError('Start your fishing first!'));
+                }
 
-        if ($form->isSubmitted() && $form->isValid()) {   
-            $activeFishing = $em->getRepository(Fishing::class)->findOneBy(['active' => true]);
+                $errors = $this->fishValidator->validate($fish);
+                foreach ($errors as $error) {
+                    $form->addError(new FormError($error));
+                }
 
-            if (!$activeFishing) {
-                throw $this->createNotFoundException('Start your fishing first!');
+                if ($form->isValid()) {
+                    $this->em->persist($fish);
+                    $this->em->flush();
+
+                    return $this->redirectToRoute('app_panel');
+                }
             }
-
-            $fish = new Fish();
-            $data = $form->getData();
-
-            $fish->setFishing($activeFishing);
-            $fish->setLength((float)$data['length']);
-            $fish->setSpecies($data['species']);
-
-            $em->persist($fish);
-            $em->flush();
-            return $this->redirectToRoute('app_panel');
-        }
 
         return $this->render('panel/fish/add.html.twig', [
             'addFishForm' => $form->createView(),
---
+++ src/Validator/FishValidator.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Validator;
+
+use App\Entity\Fish;
+use Doctrine\ORM\EntityManagerInterface;
+
+class FishValidator
+{
+    public function __construct(private EntityManagerInterface $em ){}
+
+    public function validate(Fish $fish): array
+    {
+        $errors = [];
+
+        $species = $fish->getSpecies();
+        $fishing = $fish->getFishing();
+        $today = new \DateTime();
+        $length = (float)$fish->getLength();
+        $speciesLimit = $species->getDailyLimit();
+        $fishCount = count(
+            $this->em->getRepository(Fish::class)->findBy([
+                'species' => $species,
+                'fishing' => $fishing
+            ])
+        );
+        var_dump($fishCount);
+        if ($length < $species->getMinLength()) {
+            $errors[] = 'Your fish is too small, release it!';
+        }
+
+        if (
+            $species->getProtectionStart() <= $today &&
+            $species->getProtectionEnd() >= $today
+        ) {
+            $errors[] = 'This fish is currently protected, release it!';
+        }
+        
+        if ($fishCount >= $speciesLimit) {
+            $errors[] = 'You have reached the limit, release it!';
+        }
+    
+        return $errors;
+    }
+}
